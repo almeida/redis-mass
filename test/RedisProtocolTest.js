a104132a468b7b122eb92126c4ba8496e0b69209
Thyago Almeida <ar.thyagoaa@gmail.com>
2013-09-30 08:36:02 -0300

    Fixes one char unquoted args bug

--- test/RedisProtocolTest.js
+++ test/RedisProtocolTest.js
@@ -11,5 +11,10 @@ describe('RedisProtocol', function(){
     it('should encode quoted args', function(){
       assert.equal('*2\r\n$3\r\nGET\r\n$3\r\nkey\r\n',RedisProtocol.encode('GET "key"'));
     })
+  }),
+  describe('#encode()', function() {
+    it('should encode one char unquoted args', function(){
+      assert.equal('*2\r\n$3\r\nGET\r\n$1\r\nA\r\n',RedisProtocol.encode('GET A'));
+    })
   })
 })
