c33697df9aa559dd130def96cf51e0656f7398f6
Ying Tang <ytang@godaddy.com>
2018-04-11 17:07:05 -0700

    allow endToken, especially whitespace, in quoted string value bump verion add test remove console.log can't eat the quotes....

--- test/RedisProtocolTest.js
+++ test/RedisProtocolTest.js
@@ -16,5 +16,10 @@ describe('RedisProtocol', function(){
     it('should encode one char unquoted args', function(){
       assert.equal('*2\r\n$3\r\nGET\r\n$1\r\nA\r\n',RedisProtocol.encode('GET A'));
     })
+  }),
+  describe('#encode()', function() {
+    it('should allow whitespace within quoted strings', function() {
+      assert.equal('*3\r\n$3\r\nSET\r\n$8\r\nredisKey\r\n$24\r\n{"json key 1":"value 1"}\r\n', RedisProtocol.encode('SET redisKey {"json key 1":"value 1"}'));
+    })
   })
 })
